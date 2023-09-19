f6fca4232a1d97f4bd83eeb4d96449c1edb576c2
VladAnohin <123352173+VladAnohin@users.noreply.github.com>
2023-09-19 14:58:30 +0700

    readme

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import s from './ProfileInfo.module.css'
+import React from "react";
+import s from "./ProfileInfo.module.css";
 
 const ProfileInfo = () => {
-    return (
-        <div>
-            <div>
-                <img src='https://meyerland.net/wp-content/uploads/2019/04/abandoned-forest-hd-wallpaper-34950.jpg' alt="image1" />
-            </div>
-            <div className={s.descriptionBlock}>
-                ava + description
-            </div>
-        </div>
-    )
-}
+	return (
+		<div>
+			<div>
+				<img
+					src="https://meyerland.net/wp-content/uploads/2019/04/abandoned-forest-hd-wallpaper-34950.jpg"
+					alt="image1"
+				/>
+			</div>
+			<div className={s.descriptionBlock}>
+				<p>description</p>
+			</div>
+		</div>
+	);
+};
 export default ProfileInfo;
